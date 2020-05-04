e17fa4d201c9541a3b1b7692630021ba53e896b9
Vittal Pai <vittalpai@in.ibm.com>
2020-05-04 19:50:42 +0530

    Revamp application

--- HelloLiveUpdate/ViewController.swift
+++ HelloLiveUpdate/ViewController.swift
@@ -1,5 +1,5 @@
 /**
- * Copyright 2016 IBM Corp.
+ * Copyright 2020 IBM Corp.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,31 +18,22 @@
 //  ViewController.swift
 //  HelloLiveUpdate
 //
+//
+//  Created by Vittal R Pai on 04/05/20.
+//  Copyright © 2020 IBM. All rights reserved.
+//
 
 import UIKit
 import IBMMobileFirstPlatformFoundationLiveUpdate
 
 class ViewController: UIViewController {
-    var country = "US"
-    var currentCountryButton : UIButton?
     
-    @IBOutlet weak var mapImage: UIImageView!
-    @IBOutlet weak var helloLabel: UILabel!
-    @IBOutlet weak var defaultUSButton: UIButton!
-    
-    @IBAction func selectCountry(sender: AnyObject) {
-        currentCountryButton?.isEnabled = true
-        currentCountryButton = sender as? UIButton
-        currentCountryButton?.isEnabled = false
-        country = currentCountryButton!.restorationIdentifier!
-        
-        updateHelloLabel ()
-    }
+    @IBOutlet weak var backgrounImage: UIImageView!
+    @IBOutlet weak var featureButton: UIButton!
     
     override func viewDidLoad() {
-        currentCountryButton = defaultUSButton
-        currentCountryButton?.isEnabled = false
-        updateHelloLabel ()
+        hideFeature()
+        checkForFeature()
         super.viewDidLoad()
     }
     
@@ -52,27 +43,31 @@ class ViewController: UIViewController {
         return UIImage(data: imagedData as Data, scale: 10)
     }
     
-    func updateHelloLabel () {
-        LiveUpdateManager.sharedInstance.obtainConfiguration() { (configuration, error) in
+    func checkForFeature() {
+        LiveUpdateManager.sharedInstance.obtainConfiguration(useCache: false, completionHandler: { (configuration, error) in
             if error == nil {
-                let isMapFeatureEnable = configuration!.isFeatureEnabled("includeMap")
-                if isMapFeatureEnable != nil && isMapFeatureEnable == true {
-                    if let mapUrl = configuration!.getProperty("mapUrl") {
-                        self.mapImage.image = self.getUIImage(urlString: mapUrl)
+                let isFestivalShoppingFeatureEnable = configuration!.isFeatureEnabled("festivalShopping")
+                if isFestivalShoppingFeatureEnable != nil && isFestivalShoppingFeatureEnable == true {
+                    if let imageName = configuration!.getProperty("image") {
+                        self.backgrounImage.image = UIImage(named: imageName)
+                    }
+                    if let buttonText = configuration!.getProperty("buttonLabel") {
+                        self.featureButton.isHidden = false
+                        self.featureButton.setTitle(buttonText, for: .normal)
                     }
                 } else {
-                    self.mapImage.image = nil
+                    self.hideFeature()
                 }
-                if let helloText = configuration!.getProperty("helloText") {
-                    self.helloLabel.text = helloText
-                }
-                
             } else {
-                self.mapImage.image = nil
-                self.helloLabel.text = ""
+                self.hideFeature()
                 print (error ?? "Error")
             }
-        }
+        })
+    }
+    
+    func hideFeature() {
+        self.backgrounImage.image = UIImage(named: "background")
+        self.featureButton.isHidden = true
     }
 }
 
